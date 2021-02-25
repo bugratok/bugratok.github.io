58c4601ced9e41a6873321f5353cc7d1a6500f20
bugratok <bugratok@users.noreply.github.com>
2021-02-25 04:17:27 +0300

    1.7

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,35 @@
-            document.addEventListener("DOMContentLoaded", function(event) {
-                document.documentElement.setAttribute("data-theme", "light");
+var mybutton = document.getElementById("myBtn");
+  
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+} else {
+    mybutton.style.display = "none";
+}
+}
+
+function topFunction() {
+document.body.scrollTop = 0;
+document.documentElement.scrollTop = 0;
+}
             
-                var themeSwitcher = document.getElementById("theme-switcher");
+
+document.addEventListener("DOMContentLoaded", function(event) {
+  document.documentElement.setAttribute("data-theme", "light");
             
-                themeSwitcher.onclick = function() {
-                  var currentTheme = document.documentElement.getAttribute("data-theme");
-            
-                  var switchToTheme = currentTheme === "dark" ? "light" : "dark"
-            
-                  document.documentElement.setAttribute("data-theme", switchToTheme);
-                }
-              })
+  var themeSwitcher = document.getElementById("theme-switcher");
+
+  themeSwitcher.onclick = function() {
+      var deneme = document.getElementById("post-photo").getAttribute('src');
+      var image = document.getElementById("post-photo");
+      if(deneme == "assets/make-dark.png"){
+          image.src = "assets/make-light.png";
+          document.documentElement.setAttribute("data-theme", "dark");
+      } else{
+          image.src = "assets/make-dark.png";
+          document.documentElement.setAttribute("data-theme", "light");
+      }
+  }
+})
