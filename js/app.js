eb87d465c07da7adefc65c37e173b955c4a6cdef
bugratok <bugratok@users.noreply.github.com>
2021-03-04 22:22:32 +0300

    2
    Bootstrap removed.

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@ var mybutton = document.getElementById("myBtn");
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-if (document.body.scrollTop > 350 || document.documentElement.scrollTop > 350) {
+if (document.body.scrollTop > 70 || document.documentElement.scrollTop > 70) {
     mybutton.style.display = "block";
 
 } else {
@@ -11,16 +11,17 @@ if (document.body.scrollTop > 350 || document.documentElement.scrollTop > 350) {
 }
 }
 
-function topFunction() {
-document.body.scrollTop = 0;
-document.documentElement.scrollTop = 0;
-}
+mybutton.addEventListener('click', function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+    })
+
             
 
 document.addEventListener("DOMContentLoaded", function(event) {
   document.documentElement.setAttribute("data-theme", "light");
             
-  var themeSwitcher = document.getElementById("theme-switcher");
+  var themeSwitcher = document.getElementById("post-photo");
 
   themeSwitcher.onclick = function() {
       var currentValue = document.getElementById("post-photo").getAttribute('src');
@@ -28,25 +29,10 @@ document.addEventListener("DOMContentLoaded", function(event) {
       if(currentValue == "assets/make-dark.png"){
           image.src = "assets/make-light.png";
           document.documentElement.setAttribute("data-theme", "dark");
-          console.log("It is darker now!");
       } else{
           image.src = "assets/make-dark.png";
           document.documentElement.setAttribute("data-theme", "light");
-          console.log("Sun is up!");
       }
   }
 })
-
-
-const menu = document.querySelector('.menu-button');
-
-menu.addEventListener('click', function(){
-    let clickCount = 1;
-    if (clickCount == 1){
-        menu.classList.toggle('is-active');
-    }
-    clickCount = 1;
-});
-
-
 
